cdec7d5b805bca7b639b3b85abbb8d2755cc872d
Sushmitha <58320094+Sushmitha4@users.noreply.github.com>
2025-02-08 22:05:27 +0530

    Updated to @uiw/react-codemirror

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,11 +1,9 @@
 import React, { useState }  from "react";
-import 'codemirror/lib/codemirror.css';
-import 'codemirror/theme/material.css';
-import 'codemirror/mode/xml/xml';
-import 'codemirror/mode/javascript/javascript';
-import 'codemirror/mode/css/css';
-
-import { Controlled as ControlledEditor } from 'react-codemirror2';
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+import { xml } from "@codemirror/lang-xml";
+import { css } from "@codemirror/lang-css";
+import { oneDark } from "@codemirror/theme-one-dark";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCompressAlt, faExpandAlt } from '@fortawesome/free-solid-svg-icons';
@@ -20,37 +18,52 @@ export default function Editor(props) {
 
     const [open, setOpen] = useState(true)
 
-    function handleChange(editor, data, value) {
-        onChange(value)
-    }
+    const getLanguageExtension = () => {
+        switch (language) {
+          case "javascript":
+            return javascript();
+          case "xml":
+            return xml();
+          case "css":
+            return css();
+          default:
+            return javascript();
+        }
+      };
 
     return (
-        <div className={`editor-container ${open ? '' : 'collapsed'}`}>
-            <div className="editor-title">
-                <h2>
-                    <img src="https://cpwebassets.codepen.io/assets/favicon/favicon-aec34940fbc1a6e787974dcd360f2c6b63348d4b1f4e06c77743096d55480f33.ico"/>
-                    {displayName}
-                </h2>
-                <button 
-                    type="button"
-                    className="expand-collapse-btn"
-                    onClick={() => setOpen(prevOpen => !prevOpen)}
-                >
-                    <FontAwesomeIcon icon = {open ? faCompressAlt : faExpandAlt}/>
-                </button>
-            </div>
-            <ControlledEditor 
-            onBeforeChange={handleChange}
-            value={value}
-            className="code-mirror-wrapper"
-            options={{
-                lineWrapping: true,
-                lint: true,
-                mode: language,
-                theme: 'material',
-                lineNumbers: true
-            }}
-            />
-        </div>
-    )
+        <div className={`editor-container ${open ? "" : "collapsed"}`}>
+      <div className="editor-title">
+        <h2>
+          <img
+            src="https://cpwebassets.codepen.io/assets/favicon/favicon-aec34940fbc1a6e787974dcd360f2c6b63348d4b1f4e06c77743096d55480f33.ico"
+            alt="Code Editor"
+          />
+          {displayName}
+        </h2>
+        <button
+          type="button"
+          className="expand-collapse-btn"
+          onClick={() => setOpen((prevOpen) => !prevOpen)}
+        >
+          <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
+        </button>
+      </div>
+
+      {open && (
+        <CodeMirror
+          value={value}
+          className="code-mirror-wrapper"
+          extensions={[getLanguageExtension()]}
+          theme={oneDark}
+          onChange={(val) => onChange(val)}
+          basicSetup={{
+            lineNumbers: true,
+            foldGutter: true,
+            highlightActiveLine: true,
+          }}
+        />
+      )}
+    </div>
+    );
 }
